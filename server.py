7c2ea5c0c32c578ff72b61c9944f8fff35a3c53d
Nikolay Osipov <nikolay.osipov@phystech.edu>
2020-03-26 21:25:25 +0300

    Add context manager

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
 import socket
 
-sock=socket.socket()
-sock.bind(("127.0.0.1", 10002))   # max port 65535
-sock.listen()
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
 
-conn, addr = sock.accept()
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    print(data.decode("utf8"))
-
-conn.close()
-sock.close()
+    while True:
+        conn, addr = sock.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data.decode("utf8"))
